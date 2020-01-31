3a9bf9bfc2f1a69da6a801877480981de0dd44a2
miyabi <cz1.7596hg@gmail.com>
2020-02-01 01:55:30 +0900

    ProjectAdd

---
+++ VRTest2/Assets/Scripts/SnapParts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapParts : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Rigidbody>().useGravity = false;
+        other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        other.transform.position = this.transform.position;
+        other.transform.rotation = Quaternion.Euler(30,0,0);
+    }
+}
