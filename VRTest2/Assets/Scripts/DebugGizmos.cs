6cedf3469ccd39ce68036738ba1b1f5a553f7281
Shiraeda <kurokurogmaer42@gmail.com>
2020-02-01 10:17:12 +0900

    Gizmosクラスの追加

---
+++ VRTest2/Assets/Scripts/DebugGizmos.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugGizmos : MonoBehaviour
+{
+    [SerializeField]
+    private PrimitiveType _type = PrimitiveType.Cube;
+    [SerializeField]
+    private Color _color = Color.white;
+    [SerializeField, Header("Debug")]
+    private bool _active = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!_active)
+        {
+            return;
+        }
+
+        Gizmos.color = _color;
+        Gizmos.matrix = transform.localToWorldMatrix;
+
+        Vector3 offset = Vector3.zero;
+        Vector3 size = Vector3.one;
+        float radius = 0.5f;
+        BoxCollider box = GetComponent<BoxCollider>();
+        SphereCollider sphere = GetComponent<SphereCollider>();
+        // 当たり判定の位置と大きさに合わせる
+        if(box != null)
+        {
+            offset = box.center;
+            size = box.size;
+        }
+        if(sphere != null)
+        {
+            radius = sphere.radius;
+        }
+        switch (_type)
+        {
+            case PrimitiveType.Sphere:
+                Gizmos.DrawSphere(Vector3.zero, radius);
+                break;
+            case PrimitiveType.Capsule:
+                break;
+            case PrimitiveType.Cylinder:
+                break;
+            case PrimitiveType.Cube:
+                Gizmos.DrawCube(Vector3.zero + offset, size);
+                break;
+            case PrimitiveType.Plane:
+                break;
+            case PrimitiveType.Quad:
+                break;
+            default:
+                break;
+        }
+    }
+}
