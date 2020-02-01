455b5ac790bdba078ce907bf405bd93d8099cc5b
miyabi <cz1.7596hg@gmail.com>
2020-02-01 17:52:45 +0900

    Add Assets And Ignore json And sln

---
+++ VRTest2/Assets/Scripts/TitleSceneScr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneScr : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnClickEndButton()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+        Application.Quit();
+#endif
+    }
+}
