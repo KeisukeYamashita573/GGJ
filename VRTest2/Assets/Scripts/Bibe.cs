8347e16bd4fc0bba5a77206a0befc6c75547baaa
miyabi <cz1.7596hg@gmail.com>
2020-02-01 10:44:19 +0900

    Script Add

---
+++ VRTest2/Assets/Scripts/Bibe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bibe : MonoBehaviour
+{
+    private OVRHapticsClip hapticsClip = null;
+    void Start()
+    {
+        byte[] sample = new byte[8];
+        for (int i = 0; i < sample.Length; i++)
+        {
+            sample[i] = 128;
+        }
+        hapticsClip = new OVRHapticsClip(sample, sample.Length);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            if (this.transform.parent.name.Contains("Left"))
+            {
+                OVRHaptics.LeftChannel.Mix(hapticsClip);
+            }
+            else
+            {
+                OVRHaptics.RightChannel.Mix(hapticsClip);
+            }
+        }
+    }
+}
