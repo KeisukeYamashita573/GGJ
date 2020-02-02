1c9fce54848295e0d1b1e0128f3d45ac57d33821
Shiraeda <kurokurogmaer42@gmail.com>
2020-02-02 10:58:28 +0900

    プレハブの追加 旗と花火

---
+++ VRTest2/Assets/Scripts/TextTime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTime : MonoBehaviour
+{
+    private Text _text;
+    private float _secondTime;
+    private int _minuteTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _secondTime = 0.0f;
+        _minuteTime = 0;
+    }
+
+    private void Timer()
+    {
+        _secondTime += Time.deltaTime;
+        if(_secondTime >= 60.0f)
+        {
+            _minuteTime++;
+            _secondTime -= 60.0f;
+        }
+        _text.text = _minuteTime.ToString("00")+ ":" + ((int)_secondTime).ToString("00");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+    }
+}
