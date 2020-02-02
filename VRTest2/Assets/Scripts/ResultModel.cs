c6e862fb09af5392e145aba04d8e2a2a422577b3
musubi kamishiro <kamishiro2nd.pger@gmail.com>
2020-02-02 13:40:12 +0900

    リザルト調整中

---
+++ VRTest2/Assets/Scripts/ResultModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultModel : MonoBehaviour
+{
+    private float time;
+
+    [SerializeField]
+    GameObject[] robots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        for(int i = 0; i<robots.Length;++i)
+        {
+            robots[i].SetActive(false);
+            robots[i] =Instantiate(robots[i], new Vector3(-6 + i * 3, 6.3f, 10), new Quaternion());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < robots.Length; ++i)
+        {
+            if ((time / ((i+1)*3)) >= 1)
+            {
+                robots[i].SetActive(true);
+            }
+        }
+
+        time += Time.deltaTime;
+    }
+}
