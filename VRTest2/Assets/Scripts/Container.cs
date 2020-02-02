0e9c88984416ee665558949cd324ec670aeb4734
Shiraeda <kurokurogmaer42@gmail.com>
2020-02-02 14:49:19 +0900

    メインシーンコミット

---
+++ VRTest2/Assets/Scripts/Container.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    private List<GameObject> _robotList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public List<GameObject> GetList()
+    {
+        if(_robotList == null)
+        {
+            _robotList = new List<GameObject>();
+            foreach(Transform child in transform)
+            {
+                _robotList.Add(child.gameObject);
+            }
+        }
+        return _robotList;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
